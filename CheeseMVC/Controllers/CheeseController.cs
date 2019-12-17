f4a1aa84e994942326e043ff385da755f2c08a2f
Ben Clark <brclark15@gmail.com>
2019-12-17 11:01:06 -0600

    Add CheeseMVC base app, add cheese ability, and description

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        private static Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Route("/Cheese/Add")]
+        [HttpPost]
+        public IActionResult NewCheese(string name, string description)
+        {
+            Cheeses.Add(name, description);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
